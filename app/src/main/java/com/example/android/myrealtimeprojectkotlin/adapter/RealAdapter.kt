9a2ff43333d48fd3b816c9b15b3a36ce54b0707d
User <user@Users-Macintosh.local>
2020-01-16 07:40:32 -0500

    message  adapter built

--- app/src/main/java/com/example/android/myrealtimeprojectkotlin/adapter/RealAdapter.kt
+++ app/src/main/java/com/example/android/myrealtimeprojectkotlin/adapter/RealAdapter.kt
@@ -4,13 +4,20 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.myrealtimeprojectkotlin.R
 import com.example.android.myrealtimeprojectkotlin.model.Message
+import kotlinx.android.synthetic.main.message_itemview.view.*
 
 class RealAdapter(private var messageList: List<Message>, private var applicationContext: Context) : RecyclerView.Adapter<RealAdapter.RealViewHolder>() {
 
-    class RealViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class RealViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val userName: TextView = itemView.message_username
+        val messageDate: TextView = itemView.message_date
+        val messageTitle: TextView = itemView.message_title
+        val messageContent: TextView = itemView.message_content
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RealViewHolder {
 
@@ -20,10 +27,13 @@ class RealAdapter(private var messageList: List<Message>, private var applicatio
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return messageList.size
     }
 
     override fun onBindViewHolder(holder: RealViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+       holder.userName.text = messageList[position].userName
+        holder.messageDate.text = messageList[position].messageDate
+        holder.messageTitle.text = messageList[position].messageTitle
+        holder.messageContent.text = messageList[position].messageContent
     }
 }
